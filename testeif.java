3777b43eb8505313fcb3d762de6309f5c3ae1c6e
Pedro Mello <163211156+Pedro-Mell0@users.noreply.github.com>
2024-03-20 00:30:43 +0000

    testeif.java

---
+++ testeif.java
@@ -0,0 +1,18 @@
+public class testeIf{
+public static void main(String[] args){
+
+double Numero = 10;
+    
+if (Numero > 0) {
+    System.out.println("o valor é positivo");
+}else if (Numero < 0) {
+    System.out.println("o valor é negativo");
+}else{ 
+    System.out.println(" o valor é neutro");
+    }
+
+}
+
+}
+
+
